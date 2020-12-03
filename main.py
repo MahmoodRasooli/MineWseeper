0d4e4c80c5f1924a76bc7b79d3b21eef51b7bb27
mahmood rasooli <rasuli.ut@gmail.com>
2020-12-03 21:30:34 +0330

    adding more board functionalities

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from boardForm import boardForm
-
+from board import board
 
 def main():
-    boardForm.__init__(8)
+    print(board(10)._getBombNumbers())
+    print(board(8)._getBombNumbers())
+    #boardForm.__init__(8)
 
 if __name__ == "__main__":
     main()
