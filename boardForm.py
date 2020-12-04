9d358f786689652b45df434f3fecbdee73c33d72
Mahmood Rasuli <rasuli.ut@gmail.com>
2020-12-04 20:42:48 +0330

    board form

--- boardForm.py
+++ boardForm.py
@@ -18,18 +18,25 @@ class boardForm:
         self.window.geometry('1000x900')
         self.window.configure(background="white", padx=50, pady=50)
 
-        for i in range(boardSize):
-            for j in range(boardSize):
-                text = "Bomb" if self.board.pieces[i][j].haveBomb else str(self.board.pieces[i][j].neiboursBombCount)
-                btn = ttk.Button(self.window, text=text)#.grid(row=i, column=j, padx=2, pady=2, ipadx=7, ipady=30)
-                btn.grid(row=i, column=j, padx=2, pady=2, ipadx=7, ipady=30)
+        for i_index in range(boardSize):
+            for j_index in range(boardSize):
+                icon = PhotoImage(file = "flag.png")
+                btn = ttk.Button(self.window, image=icon)#.grid(row=i, column=j, padx=2, pady=2, ipadx=7, ipady=30)
+                btn.grid(row=i_index, column=j_index, padx=2, pady=2, ipadx=7, ipady=30)
+                # btn.configure(image = icon)
                 btn.bind('<Button-1>', self.left_click)
-                btn.bind('<Button-2>', self.right_click)
-
+                btn.bind('<Button-3>', self.right_click)
+                # btn.pack()
+                
+        self.window.pack_propagate()
         self.window.mainloop()
 
         #btn = ttk.Button(window ,text="Submit").grid(row=4,column=0)
     
+    def _addBox(self, i_index: int, j_index: int):
+        # text = "Bomb" if self.board.pieces[i_index][j_index].haveBomb else str(self.board.pieces[i_index][j_index].neiboursBombCount)
+        pass
+
     def left_click(self, event):
         event.widget.configure(text = "Left Clicked")
     
